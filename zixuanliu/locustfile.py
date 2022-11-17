be8a47d81838413e8c693f87aa91814a1bb9f8cc
ZixuanLiu4869 <91376924+ZixuanLiu4869@users.noreply.github.com>
2022-11-17 02:35:51 +0200

    Delete zixuanliu directory

--- zixuanliu/locustfile.py
+++
@@ -1,9 +0,0 @@
-import time
-from locust import HttpUser, task, between
-
-class QiuckstarUser(HttpUser):
-    @task
-    def hello_world(self):
-        self.client.get('/numericalintegralservice/0/3.14159')
-        # self.client.get('/numerical?name=0_3.14159')
-
